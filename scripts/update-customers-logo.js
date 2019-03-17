faeba4058b54722df7f13ed74cc14c89e4c928de
François-Guillaume RIBREAU <github@fgribreau.com>
2019-03-17 23:14:56 +0100

    fix: fixed logo generation (do not redownload if already downloaded) and index template

--- scripts/update-customers-logo.js
+++ scripts/update-customers-logo.js
@@ -7,33 +7,33 @@ const fs = require("fs");
 const getLogoURL = domain =>
   `https://logo.clearbit.com/${domain}?size=128&greyscale=true`;
 
+const getLogoPath = domain =>
+  p.resolve(__dirname, "..", `app/img/customers/${domain}.png`);
+
 const downloadImage = customerDomain =>
-  Promise.resolve()
-    .tap(() => console.log("Downloading %s...", getLogoURL(customerDomain)))
-    .then(() =>
-      Promise.resolve(
-        request({
-          method: "GET",
-          uri: getLogoURL(customerDomain),
-          encoding: null,
-          headers: {
-            "User-Agent": "https://www.image-charts.com"
-          }
-        })
+  fs.existsSync(getLogoPath(customerDomain))
+    ? Promise.resolve().tap(() =>
+        console.log("Already downloaded %s...", getLogoURL(customerDomain))
       )
-        .tap(() => console.log("Downloaded %s logo", customerDomain))
-        .then(function(res) {
-          const buffer = Buffer.from(res, "utf8");
-          fs.writeFileSync(
-            p.resolve(
-              __dirname,
-              "..",
-              `app/img/customers/${customerDomain}.png`
-            ),
-            buffer
-          );
-        })
-    );
+    : Promise.resolve()
+        .tap(() => console.log("Downloading %s...", getLogoURL(customerDomain)))
+        .then(() =>
+          Promise.resolve(
+            request({
+              method: "GET",
+              uri: getLogoURL(customerDomain),
+              encoding: null,
+              headers: {
+                "User-Agent": "https://www.image-charts.com"
+              }
+            })
+          )
+            .tap(() => console.log("Downloaded %s logo", customerDomain))
+            .then(function(res) {
+              const buffer = Buffer.from(res, "utf8");
+              fs.writeFileSync(getLogoPath(customerDomain), buffer);
+            })
+        );
 
 Promise.mapSeries(customerDomains, downloadImage).then(
   () => console.log("done"),
