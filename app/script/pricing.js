9e17c92336cb8a045f53a75fd2ef7689219c829d
Augustin Ribreau <augustin@fgribreau.com>
2019-05-13 18:13:08 +0200

    Beginning of the text change function in pricing

---
+++ app/script/pricing.js
@@ -0,0 +1,25 @@
+function changeTextMonth() {
+  var x = document.getElementById("cost");
+  if (x.innerHTML == "529€/month") {
+    x.innerHTML = "49€/month";
+  }
+};
+
+function changeTextYear() {
+  var x = document.getElementById("cost");
+  if (x.innerHTML == "49€/month") {
+    x.innerHTML = "529€/month";
+  }
+};
+
+// const plans = [{
+// id: 'enterprise-2016',
+// price: '49.00',
+// currency: '€',
+// interval: 'monthly'
+// },{
+// id: 'enterprise-yearly-2016',
+// price: '529.00',
+// currency: '€',
+// interval: 'yearly'
+// }];
