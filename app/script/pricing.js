b7aaf8721dd504f1fbb6698f068265c59e3c0e9c
Augustin Ribreau <augustin@fgribreau.com>
2019-05-14 18:38:46 +0200

    fix: design buttons, chang some error in js, fix nommage for class or id, test in firefox and edge = OK

--- app/script/pricing.js
+++ app/script/pricing.js
@@ -1,18 +1,21 @@
-var monthButton = document.getElementById("month");
-var yearButton = document.getElementById("year");
-var cost = document.getElementById("cost");
-var urlChang = document.getElementById("urlChang");
+var monthButton = document.getElementById("pricing_by_month");
+var yearButton = document.getElementById("pricing_by_year");
+var cost = document.getElementById('pricing__price');
+var plan_subscription_url = document.getElementById("plan_subscription_url");
+var plan_id = "https://image-charts.com/enterprise/customers?plan_id=enterprise";
+// var cost = Array.from(document.querySelectorAll('.pricing__price')).forEach(el => el.innerText = "XXX€/YYY")
+
 
 monthButton.addEventListener("click", function(){
-    if (cost.innerHTML == "529€/year" && urlChang.href == "https://image-charts.com/enterprise/customers?plan_id=enterprise-yearly-2016") {
-      cost.innerHTML = "49€/month";
-      urlChang.href = "https://image-charts.com/enterprise/customers?plan_id=enterprise-2016";
-    }
+  if (cost.innerHTML == "529€/year" && plan_subscription_url.href == plan_id + "-yearly-2016") {
+    cost.innerHTML = "49€/month";
+    plan_subscription_url.href = plan_id + "-2016";
+  }
 });
 
 yearButton.addEventListener("click", function(){
-    if (cost.innerHTML == "49€/month" && urlChang.href == "https://image-charts.com/enterprise/customers?plan_id=enterprise-2016") {
+  if (cost.innerHTML == "49€/month" && plan_subscription_url.href == plan_id + "-2016") {
       cost.innerHTML = "529€/year";
-      urlChang.href = "https://image-charts.com/enterprise/customers?plan_id=enterprise-yearly-2016";
-    }
+      plan_subscription_url.href = plan_id + "-yearly-2016";
+  }
 });
