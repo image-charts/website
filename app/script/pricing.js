56e6ea4644d081e83d839e19c9797ca5dd11fb17
Augustin Ribreau <augustin@fgribreau.com>
2019-05-15 11:27:46 +0200

    fix: add value for input ; naming for subscription-url, entreprise-price and promo ; change scss by first/last-of-type

--- app/script/pricing.js
+++ app/script/pricing.js
@@ -1,21 +1,32 @@
 var monthButton = document.getElementById("pricing_by_month");
 var yearButton = document.getElementById("pricing_by_year");
-var cost = document.getElementById('pricing__price');
-var plan_subscription_url = document.getElementById("plan_subscription_url");
-var plan_id = "https://image-charts.com/enterprise/customers?plan_id=enterprise";
+var cost = document.getElementById('js-enterprise-price');
+var plan_subscription_url = document.getElementById("js-enterprise-subscription-url");
 // var cost = Array.from(document.querySelectorAll('.pricing__price')).forEach(el => el.innerText = "XXX€/YYY")
 
 
 monthButton.addEventListener("click", function(){
-  if (cost.innerHTML == "529€/year" && plan_subscription_url.href == plan_id + "-yearly-2016") {
+  if (cost.innerHTML == "529€/year" && plan_subscription_url.href == "https://image-charts.com/enterprise/customers?plan_id=enterprise-yearly-2016") {
     cost.innerHTML = "49€/month";
-    plan_subscription_url.href = plan_id + "-2016";
+    plan_subscription_url.href = "https://image-charts.com/enterprise/customers?plan_id=enterprise-2016";
   }
 });
 
 yearButton.addEventListener("click", function(){
-  if (cost.innerHTML == "49€/month" && plan_subscription_url.href == plan_id + "-2016") {
+  if (cost.innerHTML == "49€/month" && plan_subscription_url.href == "https://image-charts.com/enterprise/customers?plan_id=enterprise-2016") {
       cost.innerHTML = "529€/year";
-      plan_subscription_url.href = plan_id + "-yearly-2016";
+      plan_subscription_url.href = "https://image-charts.com/enterprise/customers?plan_id=enterprise-yearly-2016";
   }
 });
+
+// const month = {
+//   cost: '49€/month',
+//   plan_id: 'enterprise-2016',
+//   interval: 'monthly'
+// };
+//
+// const year = [{
+//   cost: '529€/year',
+//   plan_id: 'enterprise-yearly-2016',
+//   interval: 'yearly'
+// }];
