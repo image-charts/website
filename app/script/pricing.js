f26468ef7d7c60737153fec9168af8c88d375ab8
AugustinRib <augustin@fgribreau.com>
2019-06-18 21:47:03 +0200

    fix: 2/codereview

--- app/script/pricing.js
+++ app/script/pricing.js
@@ -1,18 +1,24 @@
+const planTypes = {
+    'ENTERPRISE': 'ENTERPRISE',
+    'ENTERPRISE_PLUS': 'ENTERPRISE_PLUS'
+};
 const plans = [{
+    type: planTypes.ENTERPRISE,
     cost: "49€/month",
     id: 'enterprise-2016',
     interval: 'monthly'
 }, {
+    type: planTypes.ENTERPRISE,
     cost: "529€/year",
     id: 'enterprise-yearly-2016',
     interval: 'yearly'
-}];
-
-const plansplus = [{
+}, {
+    type: planTypes.ENTERPRISE_PLUS,
     cost: "149€/month",
     id: 'plan_FG5JpvqjJwV1Uz',
     interval: 'monthly'
 }, {
+    type: planTypes.ENTERPRISE_PLUS,
     cost: "1600€/year",
     id: 'plan_FG5LuPl9Mi1fmD',
     interval: 'yearly'
@@ -24,23 +30,18 @@ const plansplus = [{
  * @property {string} interval - "yearly" | "monthly"
  */
 function displayPlan(plan) {
-    document.querySelector(".js-enterprise-price").innerHTML = plan.cost;
-    document.querySelector(".js-enterprise-subscription-url").href = `https://image-charts.com/enterprise/customers?plan_id=${plan.id}`;
-}
-
-function displayPlanplus(planplus) {
-    document.querySelector(".js-enterprise-plus-price").innerHTML = planplus.cost;
-    document.querySelector(".js-enterprise-plus-subscription-url").href = `https://image-charts.com/enterprise/customers?plan_id=${planplus.id}`;
+    document.querySelector(`.js-enterprise-price[data-type=${plan.type}]`).innerHTML = plan.cost;
+    document.querySelector(`.js-enterprise-subscription-url[data-type=${plan.type}]`).href = `https://image-charts.com/enterprise/customers?plan_id=${plan.id}`;
 }
 
 
 Array.from(document.querySelectorAll('input[name=enterprise-plan-interval]'))
     .forEach(input => input.addEventListener("change", () => {
         const selectedInterval = input.value; // "monthly" | "yearly"
-        const selectedPlan = plans.find(plan => plan.interval === selectedInterval);
-        const selectedPlanPlus = plansplus.find(planplus => planplus.interval === selectedInterval);
-        displayPlan(selectedPlan);
-        displayPlanplus(selectedPlanPlus);
+        Object.keys(planTypes).forEach((planType) => {
+            const selectedPlan = plans.find(plan => plan.interval === selectedInterval && plan.type === planTypes[planType]);
+            displayPlan(selectedPlan);
+        })
     }));
 
 
