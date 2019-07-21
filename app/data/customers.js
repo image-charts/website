e5100df66bb05153dbb10b5b677303dff972ae26
Francois-Guillaume Ribreau <github@fgribreau.com>
2019-07-21 16:04:20 +0200

    Update customers.js

--- app/data/customers.js
+++ app/data/customers.js
@@ -1,3 +1,3 @@
-module.exports = "dropbox.com,spotify.com,magento.com,discoverychannel.fr,sanebox.com,salesforce.com,gtt.net,opteo.com,redteamsecure.com,exceleron.com,gapac.com,rynekpierwotny.com,redsmin.com,xfinity.com,ampleforth.org.uk,awesomejar.com,mitsucari.com,rightrelevance.com,spothopperapp.com,podio.com,getbeyondroi.com,ccsf.edu,g2crowd.com,simplepart.com,ezyinsights.com,listen360.com".split(
+module.exports = "microsoft.com,dropbox.com,spotify.com,magento.com,discoverychannel.fr,sanebox.com,salesforce.com,gtt.net,opteo.com,redteamsecure.com,exceleron.com,gapac.com,rynekpierwotny.com,redsmin.com,xfinity.com,ampleforth.org.uk,awesomejar.com,mitsucari.com,rightrelevance.com,spothopperapp.com,podio.com,getbeyondroi.com,ccsf.edu,g2crowd.com,simplepart.com,ezyinsights.com,listen360.com".split(
   ","
 );
